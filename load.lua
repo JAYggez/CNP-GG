7c8905041410fc87f7d32a9ff214d0f01ea81f87
JAYggez <88623329+JAYggez@users.noreply.github.com>
2021-08-14 22:02:25 +0700

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,11 @@
+--BF--
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lago60/CNPHUBPAIDSCRIPT/main/Bloxfruit.lua'))()
+
+--KL--
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lago60/CNPHUBPAIDSCRIPT/main/KingLegacy.lua'))()
+
+--Plus+--
+
+loadstring(game:HttpGet('https://github.com/lago60/CNPHUBPAIDSCRIPT/blob/main/cnppaidscript.lua'))()
